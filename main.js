3acc440a3a8debf1aa2f58d8bef5dca3a7318b91
Vivek <vivekannie@yahoo.com>
2020-06-11 23:44:35 -0400

    final commit

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 //Program to allow users to edit table of text data
 //define variables in global scope
 
+
 const findInput = document.querySelector(".find-input")
 const replaceInput = document.querySelector(".replace-input")
 const replaceAllButton = document.querySelector(".replace-all-button")
@@ -16,74 +17,60 @@ const columnElements = document.querySelectorAll(".cell")
 function getCellElements (currentRowElement) {
     return currentRowElement.querySelectorAll(".cell")
 }
-// Call this function from WITHIN your row elements loop. Then you will, in turn,need to loop over the resulting cell elements. 
-//But where should this whole NESTED LOOP go? Think through the user's experience: when should WHAT happen? Requirements
-//Write a nested loop which loops over the array of cell elements.
-//For each cell element, check if a cell contains the user-provided search string. Use the string method includes().
-//If a cell does contain the user-provided search string, use innerHTML and the string method replace() to replace the user-provided search string with the user-provided replacement string.
 
 // YOUR CODE GOES HERE
-//Add a click event listener to the replaceAllButton.
-//On click but outside of a loop, assign the values of the two input textboxes ( findInput and replaceInput), so that you are only retrieving these values once per click.
+let ChangingCount=0
 replaceAllButton.addEventListener("click", function () {
-    let searchString = findInput.value
-    let replaceString = replaceInput.value
-    let tally = 0
+    let findInputValue=findInput.value
+    let replaceInputValue=replaceInput.value
     
-    console.log(searchString, replaceString)
-
-    //Write a loop which loops over the rowElements array (which is already provided in the starter code).
-    for (let rowIndex = 0; rowIndex < rowElements.length; rowIndex += 1) {
-        //Inside this loop, invoke the function getCellElements() function from above and assign the resulting array of cell elements to a variable.
-        const currentRowElement = rowElements[rowIndex]
-        let cellElements = getCellElements(currentRowElement)
-
-        // assigns extracted row to variable currentRowElement     
-        //console.log("currentRowElement is " + rowIndex + ": " + cellElements);
-        console.log("currentRowElement is " + rowIndex + ": ", cellElements);
-        //let columns = column - headers.length
-        // come back and figure out how to extract the # columns based on number of div in html
-        // console.log("you have" + column - headers.length + "columns of data in your table")
-        //let currentCellElement = ""
-
-        for (let cellIndex = 0; cellIndex < cellElements.length; cellIndex += 1) {
-            //for (let columnIndex = 0; columnIndex < 7; columnIndex += 1) {
-            const currentCellElement = cellElements[cellIndex]
-            let currentCellString = currentCellElement.innerHTML
-            console.log("columnIndex is " + cellIndex + ": ", currentCellElement);
-
-            while (currentCellString.includes(searchString)) {
-                //let newCellElement = replace(currentCellElement | (findInput.value, replaceInput.value))
-                //console.log(currentCellElement.replace(findInput, replaceInput));
-                currentCellString = currentCellString.replace(searchString, replaceString)
-                tally = tally + 1
-            }
-            currentCellElement.innerHTML = currentCellString
+    if(findInputValue.length==0||replaceInputValue.length==0){alert('Enter A Value')}
+    else{
+        for(let outerIndex=0;outerIndex<rowElements.length;outerIndex+=1){
+            let cellrows=getCellElements(rowElements[outerIndex])
+                for(let innerIndex=0;innerIndex<cellrows.length; innerIndex+=1){
+                
+                    
+                    if(cellrows[innerIndex].innerText.includes(findInputValue)){
+                    let replacement=cellrows[innerIndex].innerText
+                    replacement=replacement.replace(findInputValue,replaceInputValue)
+                    cellrows[innerIndex].innerHTML=replacement 
+                    ChangingCount+=1
+                    console.log('Number Of Changes '+ ChangingCount)
+                    
+                    }
+                }
         }
     }
-    console.log("You replaced " + tally + "items")
-    tally.innerHTML = tally
+    console.log(findInputValue)
+    console.log(replaceInputValue)
+
 })
 
 
-// One last thing: dedicate very careful attention to using variables and naming them accurately. When you change the value you are assigning to a variable, 
-// don't forget to consider changing the name to reflect the change you made! It is very easy to get confused when you are working inside NESTED LOOPS.
-// The best of us do. And unnecessary confusion during the process of developing your code means wasted time.
-// The time-cost of structuring and naming things well is FAR less than the time-cost of ignoring the quality and readability of your code. Stretch Goals
-// Display for the user the number of items replaced during the most recent replace button press. But do so with JavaScript, 
-// without changing the HTML file by hand. You can add some CSS for this.
-// In addition to the "Replace All" button, create a "Replace One" button which only replaces the first occurrence of the found search string.
-// replaceOneButton.addEventListener("click", function ()) {
-   // let searchOneString = findInput.value
-   // let replaceOneString = replaceInput.value
-   // for (let rowIndex = 0; rowIndex < rowElements.length; rowIndex += 1) {
-       // const currentRowElement = rowElements[rowIndex]
-       // for (let cellIndex = 0; cellIndex < cellElements.length; cellIndex += 1) {
-            //const currentCellElement = cellElements[cellIndex]
-            //let currentCellString = currentCellElement.innerHTML
-            //while (currentCellString.includes(searchString)) {
-               // currentCellString = currentCellString.replace(searchString, replaceString)
-            //}
-// Add a checkbox which toggles case-insensitive searches.
-// Research and use LocalStorage and JSON.stringify() to store a history of the user-provided search strings 
-// which will persist beyond page refreshes. On page load, display any past search history in a console.log.
+replaceOneButton.addEventListener("click", function () {
+    let findInputValue=findInput.value
+    let replaceInputValue=replaceInput.value
+    
+    if(findInputValue.length==0||replaceInputValue.length==0){alert('Enter A Value')}
+    else{
+        for(let outerIndex=0;outerIndex<rowElements.length;outerIndex+=1){
+            let cellrows=getCellElements(rowElements[outerIndex])
+                for(let innerIndex=0;innerIndex<cellrows.length; innerIndex+=1){
+                
+                    
+                    if(cellrows[innerIndex].innerText.includes(findInputValue)){
+                    let replacement=cellrows[innerIndex].innerText
+                    replacement=replacement.replace(findInputValue,replaceInputValue)
+                    cellrows[innerIndex].innerHTML=replacement 
+                    outerIndex=rowElements.length
+                    ChangingCount+=1
+                    console.log('Number Of Changes '+ ChangingCount)
+                    
+                    }
+                }
+        }
+    }
+    console.log(findInputValue)
+    console.log(replaceInputValue)
+})
